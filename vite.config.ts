e2055e7ae550afba90cb540eca50bc92b1608177
Jorge Carmona <jorgecarmona1986@gmail.com>
2025-09-26 08:53:57 -0300

    okei

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/ ...
 export default defineConfig({
   plugins: [vue()],
-  base: '/app-vue-reto/'
+  base: 'https://vision-st.github.io/app-vue-reto'
 })
